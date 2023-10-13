0e6571b11357ef5dfaa9a43d89268025127c9758
Maria Aufschlager <aufschlager.maria@gmail.com>
2023-10-13 12:13:02 +0200

    lost datetime attribute

--- data_collection/stepstone_data_engineer.py
+++ data_collection/stepstone_data_engineer.py
@@ -14,9 +14,13 @@ requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
 
-url = 'https://www.stepstone.de/jobs/data-engineer/in-berlin?radius=5'
+url = 'https://www.stepstone.de/jobs/in-germany?radius=30&action=facet_selected%3bskills%3b673826&fsk=673826'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0)'}
 
 response = requests.get(url, headers=headers)
 
 #print(response.text)
+
+soup = bs(response.content, 'html.parser')
+
+print(soup.find('time'))
