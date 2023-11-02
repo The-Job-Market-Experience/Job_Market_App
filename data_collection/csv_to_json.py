c50b98883c0ed6e505a0558e04f4c0ccc29d0091
Maria Aufschlager <aufschlager.maria@gmail.com>
2023-11-02 09:44:13 +0100

    updated file

--- data_collection/csv_to_json.py
+++ data_collection/csv_to_json.py
@@ -6,7 +6,7 @@ jobs_json = 'jobs_dict.json'
 ## read csv file and add to data
 
 data = {}
-with open(jobs_csv, encoding='utf8') as csvFile:
+with open(jobs_csv, encoding='utf-8') as csvFile:
     csvReader = csv.DictReader(csvFile)
     for rows in csvReader:
         key = rows[""]
@@ -14,5 +14,5 @@ with open(jobs_csv, encoding='utf8') as csvFile:
 
 ## create new json file and write data in it
 
-with open(jobs_json, 'w', encoding='utf8') as jsonFile:
+with open(jobs_json, 'w', encoding='utf-8') as jsonFile:
     jsonFile.write(json.dumps(data, indent=4))
